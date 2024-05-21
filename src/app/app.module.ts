527f2dd3c82770cc2d69306272a59f3fedcae31a
cutiBilotta <agus.bilotta@gmail.com>
2024-05-21 17:21:10 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,36 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DeviceMotion } from '@ionic-native/device-motion/ngx';
+import { Vibration } from '@ionic-native/vibration/ngx';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKyiOTrJYW7wt8H8L7pRgvx1CgHy6--mo",
+  authDomain: "pruebaapp-303bb.firebaseapp.com",
+  projectId: "pruebaapp-303bb",
+  storageBucket: "pruebaapp-303bb.appspot.com",
+  messagingSenderId: "694456344034",
+  appId: "1:694456344034:web:8dc046ff25b65273572dbb"
+};
+
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule],
+  providers: [
+
+    DeviceMotion,
+    Vibration,
+    Flashlight, ScreenOrientation,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,179 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
+import { Flashlight } from '@ionic-native/flashlight/ngx';
+import { AuthService } from 'src/app/services/auth.service';
+import { Vibration } from '@ionic-native/vibration/ngx';
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-admin',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  countPasswordWrong = 0;
+  estado = "";
+  showDialog: boolean = false;
+  password = '';
+  presionado: boolean = false;
+  subscription: any;
+  audioIzquierda = "../../assets/audios/izquierda.mp3";
+  audioDerecha = "../../assets/audios/derecha.mp3";
+  audioFlash = "../../assets/audios/alarmaDos.wav";
+  audioVibrando = "../../assets/audios/vibrando.wav";
+  invalidPassword = "../../assets/audios/alarmaUno.wav";
+  audio = new Audio();
+  primerIngreso: boolean = true;
+  primerIngresoFlash: boolean = true;
+
+  posicionActual = 'actual';
+  posicionAnterior = 'anterior';
+  mostrarDialog: boolean = true;
+  // TEST
+  accelerationX: any;
+  accelerationY: any;
+  accelerationZ: any;
+
+  constructor(private screenOrientation: ScreenOrientation,
+    private deviceMotion: DeviceMotion,
+    private flashlight: Flashlight,
+    private authService: AuthService,
+    private vibration: Vibration
+  ) {
+    // this.audioIzquierda = new Audio("../../assets/audios/estaHurtando.wav");
+    // this.audioDerecha = new Audio("../../assets/audios/esMio.wav");
+    // this.audioFlash = new Audio("../../assets/audios/alarmaDos.wav");
+    // this.audioVibrando = new Audio("../../assets/audios/vibrando.wav");
+    // this.invalidPassword = new Audio("../../assets/audios/alarmaUno.wav");
+  }
+
+  ngOnInit() {
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT_PRIMARY);
+  }
+
+  activarDesactivarAlarma() {
+    if (this.presionado) {//si esta presionado entro para mostrar el dialog de deslogeo
+      this.showDialog = true;
+      if (this.password == this.authService.currentUser.password) {//verifico que la contraseña sea correcta
+        this.estado = "permitido"// muestro la doble tilde del html
+        setTimeout(() => {
+          this.presionado = false; //cambio el estado del boton
+          this.estado = ""//muestro el icono de ingreso en html
+          this.password = "";//borro el password de ingreso
+          this.showDialog = false;//oculto el dialog
+          this.stop();//dejo de estar subscripto al acceleration
+          this.countPasswordWrong = 0;//reinicio el contador
+        }, 1000);
+      }
+      else if (this.password != '') {
+        this.countPasswordWrong++;
+        this.estado = "denegado"// muestro la doble tilde del html
+        setTimeout(() => {
+          this.estado = ""//muestro el icono de ingreso en html
+        }, 1000);
+        if (this.countPasswordWrong > 2) {
+          this.audio.src = this.invalidPassword;
+          this.audio.play();
+        }
+      }
+    }
+    else {
+      this.presionado = true;
+      this.start();
+    }
+  }
+  closeDialog() {
+    this.showDialog = false;
+  }
+
+  activarSoloAudioIzquierda() {
+    //desactivamos otros audios
+    this.primerIngreso = false;
+    this.primerIngresoFlash = true;
+    //activamos el audio que queremos
+    //this.audioIzquierda.play(); //play
+    if ((this.posicionActual!=this.posicionAnterior)) {
+          this.posicionAnterior="izquierda";
+          this.audio.src = this.audioIzquierda;
+    }
+    this.audio.play();
+  }
+  activarSoloAudioDerecha() {
+    this.primerIngreso = false;
+    this.primerIngresoFlash = true;
+    if (this.posicionActual!=this.posicionAnterior) {
+      this.posicionAnterior="derecha";
+      this.audio.src = this.audioDerecha;
+    }
+    this.audio.play();
+  }
+  activarSoloAudioConVibrador() {
+    if (this.posicionActual!=this.posicionAnterior){
+      this.posicionAnterior="plano";
+
+      this.audio.src = this.audioVibrando;
+    }
+    this.primerIngreso ? null : this.audio.play();
+    this.primerIngreso ? null : this.vibration.vibrate(5000);
+    this.primerIngreso = true;
+    this.primerIngresoFlash = true;
+  }
+  activarSoloFlashConAudio() {
+    if (this.primerIngresoFlash) {
+      this.primerIngresoFlash ? this.flashlight.switchOn() : null;
+      setTimeout(() => {
+        this.primerIngresoFlash = false;
+        this.flashlight.switchOff();
+      }, 5000);
+      this.primerIngreso = false;
+    }
+
+  }
+  reiniciarFlashYVibrador() {
+    this.primerIngreso = false;
+    this.primerIngresoFlash = true;
+  }
+  start() {
+    this.subscription = this.deviceMotion.watchAcceleration({ frequency: 300 }).subscribe((acceleration: DeviceMotionAccelerationData) => {
+      this.accelerationX = Math.floor(acceleration.x);
+      this.accelerationY = Math.floor(acceleration.y);
+      this.accelerationZ = Math.floor(acceleration.z);
+
+      if (acceleration.x > 5) {
+        //Izquierda
+        this.posicionActual='izquierda';
+        this.activarSoloAudioIzquierda();
+      }
+      else if (acceleration.x < -5) {
+        //derecha
+        this.posicionActual='derecha';
+        this.activarSoloAudioDerecha();
+      }
+      else if (acceleration.y >= 9) {
+        //encender flash por 5 segundos y sonido
+        this.posicionActual='arriba';
+        if ((this.posicionActual!=this.posicionAnterior)) {
+          this.audio.src = this.audioFlash;
+          this.posicionAnterior="arriba";
+        }
+        this.audio.play();
+        this.activarSoloFlashConAudio();
+
+      }
+      else if (acceleration.z >= 9 && (acceleration.y >= -1 && acceleration.y <= 1) && (acceleration.x >= -1 && acceleration.x <= 1)) {
+        //acostado vibrar por 5 segundos y sonido
+        this.posicionActual='plano';
+        this.activarSoloAudioConVibrador();
+
+      }
+
+    });
+  }
+
+  stop() {
+    this.mostrarDialog = true;
+    this.primerIngreso = true;
+    this.subscription.unsubscribe();
+  }
 
 }
